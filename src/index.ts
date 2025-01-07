c86b84953a7dc2c4753fed119f51f33191ef51aa
Ricardo <ricardo.machado.nwi@gmail.com>
2025-01-07 16:51:56 -0300

    feat: open calendar window

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import { BasicApp } from "./view/basic/BasicApp"
+import "../styles/custom-calendar-ui.css"
 
+const calendarApp = new BasicApp()
 // Creates and renders BasicApp on the Foundry `ready` hook.
 Hooks.on("ready", () => {
-   new BasicApp().render(true, { focus: true })
+   // calendarApp.render(true, { focus: true })
+})
+
+Hooks.on('renderHotbar', (_, hotbarElement :JQuery<HTMLElement>)=> {
+   hotbarElement.append(
+      /*html*/
+      `
+        <button
+          type='button'
+          class='cc-custom-calendar-ui-button'
+          title='Custom Calendar'
+        >
+            <i class="fa-regular fa-calendar-days fa-2x"></i>
+        </button>
+      `
+    )
+
+    hotbarElement.on("click", '.cc-custom-calendar-ui-button', (_) => {
+      calendarApp.render(true, { focus: true })
+    })
+
 })
